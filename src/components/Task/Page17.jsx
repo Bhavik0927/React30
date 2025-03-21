65a2d97457ce80fc8551bfd71b1f764b5f796c84
Bhavik <bhaviktembhare2@gmail.com>
2025-03-21 16:08:08 +0530

    after using usememo

--- src/components/Task/Page17.jsx
+++ src/components/Task/Page17.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
-const Users = Array.from({length:1000}, (_, i) => `User => ${i + 1}`);
+const Users = Array.from({ length: 1000 }, (_, i) => `User => ${i + 1}`);
 
 const Page17 = () => {
     const [search, setSearch] = useState("");
 
-    const filterUser = Users.filter(user =>{
-        return user.toLowerCase().includes(search.toLowerCase());
-    })
+    const filterUser = useMemo(() => {
+        console.log("Filtering users...");
+        return Users.filter(user =>
+            user.toLowerCase().includes(search.toLowerCase())
+        )
+    }, [search])
 
     return (
         <div className="p-4 h-lvh flex items-center justify-center flex-col">
             <input
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => setSearch(e.target.value)} />
+                type="text"
+                placeholder="Search..."
+                onChange={(e) => setSearch(e.target.value)} />
 
             <ul>
                 {
-                    filterUser.slice(0,10).map((user,index) =>(
+                    filterUser.slice(0, 10).map((user, index) => (
                         <li key={index}>{user}</li>
                     ))
                 }
